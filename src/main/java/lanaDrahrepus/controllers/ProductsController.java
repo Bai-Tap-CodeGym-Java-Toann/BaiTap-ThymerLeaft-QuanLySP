cb6c11852c2f1b372188feff8a1c31bbadfb5e7c
Nguyễn Đại Toàn <sitdownrightnow2552@gmail.com>
2018-10-26 20:30:35 +0700

    halfdone-edit

--- src/main/java/lanaDrahrepus/controllers/ProductsController.java
+++ src/main/java/lanaDrahrepus/controllers/ProductsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ProductsController {
@@ -37,12 +38,27 @@ public class ProductsController {
     }
 
     @PostMapping("/products/edit/{code}")
-    public String edit(@RequestParam Model model,
-                       Object editor,
-                       @PathVariable
-                               int code) {
-        Product product = new Product(code, editor.name,editor.price,editor.origin);
-        products.update(code,)
-
+    public String edit(@RequestParam
+                               String name,
+                       String price,
+                       String origin,
+                       String picture,
+                       Model model,
+                       RedirectAttributes redirect,
+                       @PathVariable int code) {
+        Product oldProduct = products.find(code);
+        try {
+            Double.parseDouble(price);
+        } catch (Throwable t) {
+            String error ="giá không hợp lệ";
+            model.addAttribute("error", error);
+            model.addAttribute("product", oldProduct);
+            return "redirect:/products/edit/"+code;
+        }
+        Product product = new Product(code, name, Double.parseDouble(price), origin, picture);
+        if (products.update(code, product)) {
+            return "redirect:/products";
+        }
+        return "error404";
     }
 }
